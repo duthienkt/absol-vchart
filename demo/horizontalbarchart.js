cf07307ab39952125564b979a2c08c184e4141fc
duthienkt <blueskythien2010@live.com>
2019-11-11 18:20:47 +0700

    - update demo

---
+++ demo/horizontalbarchart.js
@@ -0,0 +1,21 @@
+var skillChartProps = {
+    canvasWidth: 900,
+    canvasHeight: 600,
+    keys: ['Bạn', 'Quản lý', 'Cấp trên', 'Đồng nghiệp', 'Cấp dưới', 'Khách hàng', 'Mọi người'],
+    bars: [4.2, 2.3, 4.5, 4.4, 3.6, 2.5, 4.3],
+    ranges:[
+        [3.7, 4.5],
+        [1.5, 2.9],
+        [3.3, 4.8],
+        [4.3, 4.5],
+        [2.2, 4.3],
+        [1.1, 3.3],
+        [4, 5]
+    ]
+};
+
+vchart._({
+    tag: 'horizontalbarchart',
+    props: skillChartProps
+
+}).addTo(document.body);
