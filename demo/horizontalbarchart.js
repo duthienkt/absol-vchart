9603566a2ff4e063fa10b902910bf7ecc73570e9
duthienkt <blueskythien2010@live.com>
2019-11-12 10:30:35 +0700

    - demo

--- demo/horizontalbarchart.js
+++ demo/horizontalbarchart.js
@@ -3,8 +3,8 @@ var skillChartProps = {
     canvasHeight: 600,
     keys: ['Bạn', 'Quản lý', 'Cấp trên', 'Đồng nghiệp', 'Cấp dưới', 'Khách hàng', 'Mọi người'],
     bars: [4.2, 2.3, 4.5, 4.4, 3.6, 2.5, 4.3],
-    title:'Mô tả năng lực ABC',
-    ranges:[
+    title: 'Mô tả năng lực ABC',
+    ranges: [
         [3.7, 4.5],
         [1.5, 2.9],
         [3.3, 4.8],
@@ -12,11 +12,20 @@ var skillChartProps = {
         [2.2, 4.3],
         [1.1, 3.3],
         [4, 5]
+    ],
+    vLines: [
+        {
+            value: 1.5,
+            color: 'red'
+        },
+        {
+            color: 'blue',
+            value: 3.5
+        }
     ]
 };
 
 vchart._({
     tag: 'horizontalbarchart',
     props: skillChartProps
-
 }).addTo(document.body);
