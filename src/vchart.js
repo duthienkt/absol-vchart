b582ca00e6a634735d278756af59eefe016d4581
duthienkt <blueskythien2010@live.com>
2019-12-24 14:39:21 +0700

    - new structor

--- src/vchart.js
+++ src/vchart.js
@@ -7,17 +7,21 @@ Object.assign(vchart, helper);
 vchart.tl = tl;
 
 if ('absol' in window) {
-    if (absol.ShareCreator) {
-        Object.assign(
-            absol.ShareCreator,
-            Object.keys(vchart.creator)
-                .filter(function (e) {
-                    return !!e.match(/.+chart/);
-                }).reduce(function (ac, cr) {
-                    ac[cr] = vchart.creator[cr];
-                    return ac;
-                }, {})
-        );
+    if (absol.coreDom) {
+        absol.coreDom.install(
+            Object.assign(
+                absol.ShareCreator,
+                Object.keys(vchart.creator)
+                    .filter(function (e) {
+                        return !!e.match(/.+chart/);
+                    }).reduce(function (ac, cr) {
+                        ac[cr] = vchart.creator[cr];
+                        return ac;
+                    }, {})
+            ))
+    }
+    else {
+        console.error("coreDom not found");
     }
 }
 
