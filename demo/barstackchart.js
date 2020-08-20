81ad1105418063f3282f9923538c5131cb805723
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-08-20 13:32:23 +0700

    - demo

---
+++ demo/barstackchart.js
@@ -0,0 +1,29 @@
+absol._({
+    tag: 'barstackchart',
+    style: {
+        width: '80vw',
+        height: 'calc(100vh - 100px)',
+        'box-shadow': '0px 0px 0px 1px rgba(0,0,0,0.75)'
+    },
+    props: {
+        title: 'App Conversions',
+        blocks: [
+            {
+                name: 'Visit',
+                value: 100
+            },
+            {
+                name: 'Sign-up',
+                value: 75
+            },
+            {
+                name: 'Purchase',
+                value: 55
+            },
+            {
+                name: "Review",
+                value: 22
+            }
+        ]
+    }
+}).addTo(document.body);
