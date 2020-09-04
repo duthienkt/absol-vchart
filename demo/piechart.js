5f10f822b0dabeddef8fbd6e9177d2667cb99fd3
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-09-04 11:48:16 +0700

    - special case demo

--- demo/piechart.js
+++ demo/piechart.js
@@ -93,7 +93,44 @@ absol._({
             {
                 name: 'Other',
                 value: 11,
-                valueText: '11%'                ,
+                valueText: '11%',
+                separated: true
+            },
+            {
+                name: 'Chrome',
+                value: 61,
+                valueText: '61%'
+            }
+        ]
+    }
+}).addTo(document.body);
+
+absol._({
+    tag: 'piechart',
+    style: {
+        width: '40vw',
+        height: '60vh',
+        minWidth: '300px',
+        minHeight: '500px'
+    },
+    props: {
+        title: 'Browser',
+        pieces: [
+            {
+                name: 'IE',
+                value: 0,
+                valueText: '',
+            },
+            {
+                name: 'Firefox',
+                value: 0,
+                valueText: '',
+                separated: true
+            },
+            {
+                name: 'Other',
+                value: 0,
+                valueText: '',
                 separated: true
             },
             {
