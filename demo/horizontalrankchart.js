ca6b2c7f228be3d145238bfd2c9f15658265504d
duthienkt <blueskythien2010@live.com>
2024-07-27 01:07:35 +0700

    - demo

---
+++ demo/horizontalrankchart.js
@@ -0,0 +1,70 @@
+var y = vchart._({
+    tag: 'horizontalrankchart',
+    style:{
+      width:'80vw',
+      height: 'auto'
+    },
+    props: {
+        resizable: true,
+        title: 'Biểu đồ bậc lương',
+        zeroOY: true,
+        valueName: 'lương(VND)',
+        numberToString: vchart.lambda.toLocalString(0),
+        positions: [
+            {
+                name: 'Giám đốc',
+                ranks: [13000000, 15000000, 16500000, 17700000, 19000000, 20900000, 21000000, 21500000]
+            },
+            {
+                name: 'Trưởng phòng tài chính',
+                ranks: [15000000, 17000000, 17500000, 18500000, 19700000, 20000000, 21000000, 21500000, 21700000]
+            },
+            {
+                name: 'Trưởng phòng nhân sự',
+                ranks: [13000000, 14000000, 15500000, 16500000, 17700000, 18000000, 19000000, 20500000, 21700000]
+            },
+            {
+                name: 'Trưởng phòng kế toán',
+                ranks: [11000000, 11200000, 13500000, 14100000, 15000000, 16100000, 17000000, 18500000, 19700000
+                    , 20000000, 21000000, 22000000, 22500000, 23500000]
+            }
+        ]
+    }
+}).addTo(document.body);
+vchart.addDevContextMenu(y);
+var x = vchart._({
+    tag: 'horizontalrankchart',
+    style:{
+        width: '100%',
+        height: 'auto'
+    },
+    props: {
+        resizable: true,
+        title: 'Biểu đồ  bậc lương',
+        zeroOY: true,
+        valueName: 'lương(VND)',
+        keyName: 'chức danh',
+        numberToString: vchart.lambda.toLocalString(0),
+        positions: [
+            {
+                name: 'Giám đốc',
+                ranks: [13000000, 15000000, 16500000, 17700000, 19000000, 20900000, 21000000, 21500000]
+            },
+            {
+                name: 'Trưởng phòng tài chính',
+                ranks: [15000000, 17000000, 17500000, 18500000, 19700000, 20000000, 21000000, 21500000, 21700000]
+            },
+            {
+                name: 'Trưởng phòng nhân sự',
+                ranks: [13000000, 14000000, 15500000, 16500000, 17700000, 18000000, 19000000, 20500000, 21700000]
+            },
+            {
+                name: 'Trưởng phòng kế toán',
+                ranks: [11000000, 11200000, 13500000, 14100000, 15000000, 16100000, 17000000, 18500000, 19700000]
+            }
+
+        ]
+    }
+
+}).addTo(document.body);
+vchart.addDevContextMenu(x);
